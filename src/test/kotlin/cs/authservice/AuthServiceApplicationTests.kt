ab0f898541ebdca1d9bcdae693e4077cd274665f
senemogluc <cagin.senemoglu@gmail.com>
2023-07-11 12:14:02 +0300

    initial commit

---
+++ src/test/kotlin/cs/authservice/AuthServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package cs.authservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
